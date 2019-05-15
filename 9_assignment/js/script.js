7bdf7591c5422143d2a22c7e15be7c0c769704de
Jayashree20196 <njayashree20196@gmail.com>
2019-05-15 16:24:49 +0530

    Added Assignment 9

---
+++ 9_assignment/js/script.js
@@ -0,0 +1,19 @@
+$("#owl-demo").owlCarousel({
+  navigation : true, 
+  slideSpeed : 300,
+  paginationSpeed : 500,
+  items: 1,
+  singleItem: true,
+  autoPlay : 4000
+});
+
+$("#owl-demo-2").owlCarousel({
+  navigation : true, 
+  animateOut: 'fadeOut',
+  slideSpeed : 300,
+  dots: true,
+  paginationSpeed : 500,
+  items: 1,
+  singleItem: true,
+  autoPlay : 4000
+});
